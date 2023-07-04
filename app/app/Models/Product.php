bab0214af1df841294649a7875bb0fd7e4d77195
edvaldotorres <edvaldo.torres@tisaude.com>
2023-07-04 01:56:57 -0300

    document: add comment the model product

--- app/app/Models/Product.php
+++ app/app/Models/Product.php
@@ -10,6 +10,11 @@ class Product extends Model
 {
     use HasFactory, SoftDeletes;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
         'name',
         'description',

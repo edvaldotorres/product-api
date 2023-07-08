2fa4c4b1faa23489e327c9645c51d3fbc84efef4
edvaldotorres <edvaldo.torres@tisaude.com>
2023-07-08 16:29:13 -0300

    feature: add new feature search

--- app/routes/api.php
+++ app/routes/api.php
@@ -18,4 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
     Route::apiResource('products', ProductController::class);
+    
+    Route::get('products/view/search', [ProductController::class, 'search']);
 });

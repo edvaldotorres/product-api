227b80f48a6352dd36579b56b3a4dbd3e6eafc71
edvaldotorres <edvaldo.torres@tisaude.com>
2023-07-08 16:28:55 -0300

    refactor: add product interface and repository the controllers

--- app/app/Http/Controllers/Api/V1/ProductController.php
+++ app/app/Http/Controllers/Api/V1/ProductController.php
@@ -5,65 +5,111 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\ProductRequest;
 use App\Http\Resources\Api\V1\ProductResource;
-use App\Models\Product;
+use App\Repositories\ProductRepository;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
     /**
-     * @var Product
+     * The constructor function initializes a private property  with an instance of the
+     * ProductRepository class.
      */
-    private $product;
-
-    public function __construct(Product $product)
+    public function __construct(private ProductRepository $productRepository)
     {
-        $this->product = $product;
     }
 
     /**
-     * Display a listing of the resource.
+     * The index function retrieves all products from the product repository and returns them as a
+     * collection of product resources.
+     * 
+     * @return AnonymousResourceCollection an instance of the `AnonymousResourceCollection` class.
      */
-    public function index()
+    public function index(): AnonymousResourceCollection
     {
-        $products = $this->product->paginate(10);
+        $products = $this->productRepository->getAll();
         return ProductResource::collection($products);
     }
 
     /**
-     * Store a newly created resource in storage.
+     * The search function in PHP takes a search term and a number of results per page, searches for
+     * products matching the search term, and returns a collection of product resources.
+     * 
+     * @param Request request The `` parameter is an instance of the `Illuminate\Http\Request`
+     * class. It represents the current HTTP request made to the server and contains information such as
+     * the request method, headers, query parameters, and request body.
+     * 
+     * @return AnonymousResourceCollection an AnonymousResourceCollection of ProductResource objects.
      */
-    public function store(ProductRequest $request)
+    public function search(Request $request): AnonymousResourceCollection
     {
-        $product = $this->product->create($request->validated());
+        $data = $this->productRepository->searchProduct($request->search, $request->perPage);
+        return ProductResource::collection($data);
+    }
+
+    /**
+     * The store function creates a new product using the validated data from the request and returns a
+     * resource representation of the created product.
+     * 
+     * @param ProductRequest request The `` parameter is an instance of the `ProductRequest` class.
+     * It is used to validate and retrieve the data sent in the request. In this case, it is used to
+     * validate the data and retrieve the validated data using the `validated()` method.
+     * 
+     * @return ProductResource The store() function is returning an instance of the ProductResource class.
+     */
+    public function store(ProductRequest $request): ProductResource
+    {
+        $product = $this->productRepository->create($request->validated());
         return ProductResource::make($product);
     }
 
     /**
-     * Display the specified resource.
+     * The function "show" retrieves a product by its ID and returns it as a ProductResource object.
+     * 
+     * @param string id The "id" parameter is a string that represents the unique identifier of a product.
+     * It is used to retrieve the product from the product repository.
+     * 
+     * @return ProductResource an instance of the `ProductResource` class.
      */
-    public function show(string $id)
+    public function show(string $id): ProductResource
     {
-        $product = $this->product->findOrFail($id);
+        $product = $this->productRepository->getByID($id);
         return ProductResource::make($product);
     }
 
     /**
-     * Update the specified resource in storage.
+     * The function updates a product with the given ID using the validated data from the request and
+     * returns a resource representation of the updated product.
+     * 
+     * @param ProductRequest request The `` parameter is an instance of the `ProductRequest` class.
+     * It is used to validate and retrieve the data sent in the request.
+     * @param string id The `id` parameter is a string that represents the unique identifier of the product
+     * that needs to be updated.
+     * 
+     * @return ProductResource The method is returning an instance of the `ProductResource` class.
      */
-    public function update(ProductRequest $request, string $id)
+
+    public function update(ProductRequest $request, string $id): ProductResource
     {
-        $product = $this->product->findOrFail($id);
-        $product->update($request->validated());
+        $product = $this->productRepository->getByID($id);
+        $this->productRepository->update($product->id, $request->validated());
 
         return ProductResource::make($product);
     }
 
     /**
-     * Remove the specified resource from storage.
+     * The "destroy" function deletes a product from the database based on its ID.
+     * 
+     * @param string id The `id` parameter is a string that represents the unique identifier of the product
+     * that needs to be deleted.
+     * 
+     * @return Response a `Response` object.
      */
-    public function destroy(string $id)
+    public function destroy(string $id): Response
     {
-        $product = $this->product->findOrFail($id);
-        $product->delete();
+        $product = $this->productRepository->getByID($id);
+        $this->productRepository->delete($product->id);
 
         return response()->noContent();
     }
